4b01fafe300807ef12452a739f21866a3aaa47a3
Andrii <andriy.lavruk.ua@gmail.com>
2022-12-08 22:00:11 +0200

    Created models Expense and ExpenseCategory.

---
+++ MoneyApp/MoneyApp/Shared/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyApp.Shared.Models;
+
+public class Expense
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime DateCreated { get; set; }
+    public ExpenseCategory? ExpenseCategory { get; set; }
+    public Guid ExpenseCategoryId { get; set; }
+}
---
+++ MoneyApp/MoneyApp/Shared/Models/ExpenseCategory.cs
@@ -0,0 +1,7 @@
+﻿namespace MoneyApp.Shared.Models;
+
+public class ExpenseCategory
+{
+    public Guid Id { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+}
